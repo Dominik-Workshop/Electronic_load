70c64d52dadc3fa4cca14eb750971dd6b7ebb220
Dominik Pluta <nmarcin87@gmail.com>
2022-07-27 02:16:56 +0200

    init function - pinMode

---
+++ Electronic_load_code/include/init.hh
@@ -0,0 +1,20 @@
+/**
+ * @file init.hh
+ * @author Dominik Workshop
+ * @brief initializes all used pins
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef INIT_HH
+#define INIT_HH
+
+#include <Arduino.h>
+#include "defines.hh"
+
+void pinInit();
+
+#endif
---
+++ Electronic_load_code/src/init.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file init.cpp
+ * @author Dominik Workshop
+ * @brief initializes all used pins
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "init.hh"
+
+/**
+ * @brief initializes input and output pins
+ * 
+ */
+void pinInit(){
+  pinMode(TEMPERATURE, INPUT);
+  pinMode(FAN, OUTPUT);
+  pinMode(OUTPUT_OFF, OUTPUT);
+  pinMode(IN_TRIGGER, INPUT);
+}
+
