b725f7de8c05cc9d265cc6a944768e7d6924ef56
Dominik Pluta <nmarcin87@gmail.com>
2022-07-27 02:21:12 +0200

    initialized adc, dac, lcd, declared calValues

--- Electronic_load_code/src/main.cpp
+++ Electronic_load_code/src/main.cpp
@@ -1,9 +1,50 @@
+/**
+ * @file main.cpp
+ * @author Dominik Workshop
+ * @brief Electronic load with 4 different modes of operation:
+ *  - Constant current,
+ *  - Constant power,
+ *  - Constant resistance,
+ *  - Transient response
+ * @version 0.1
+ * @date 2022-07-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+#include <Adafruit_MCP4725.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+
+#include "defines.hh"
+#include "init.hh"
+#include "calibration.hh"
 
 void setup() {
-  // put your setup code here, to run once:
+  CalibrationValues calibrationValues;
+  LiquidCrystal_I2C lcd(LCD_ADDRESS, 20, 4);
+  Adafruit_ADS1115 adc;
+  Adafruit_MCP4725 dac;
+
+  lcd.init();  
+  adc.begin();
+  adc.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.0078125mV
+  dac.begin(DAC_ADDRESS);
+
+
+  pinInit();
+  calibrationValues.readFromEEPROM();
+
+  // Print a message to the LCD.
+  //lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+
 }
