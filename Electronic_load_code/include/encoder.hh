a4239e6fc06d7968d81319609d8b5cf9dbe3d3c6
Dominik Pluta <nmarcin87@gmail.com>
2022-08-02 01:22:44 +0200

    encoder class with interrupt

--- Electronic_load_code/include/encoder.hh
+++ Electronic_load_code/include/encoder.hh
@@ -15,21 +15,47 @@
 #include <Arduino.h>
 #include "defines.hh"
 
-volatile int EncoderValue = 0;
+class Encoder{
+  static void isr();
+  static Encoder * instance;
 
-void isr(){
+  void handleInterrupt ();
+
+  volatile int counter = 0;
+
+  public:
+    void begin ();
+    int getCounts();
+};
+
+void Encoder::begin(){
+    attachInterrupt(digitalPinToInterrupt(2), isr, LOW);
+    instance = this;
+}
+void Encoder::isr()
+  {
+    instance->handleInterrupt();
+  }
+
+Encoder * Encoder::instance;
+
+void Encoder::handleInterrupt(){
   static unsigned long lastInterruptTime = 0;
   unsigned long interruptTime = millis();
 
   // If interrupts come faster than 5ms, assume it's a bounce and ignore
   if(interruptTime - lastInterruptTime > 5){
     if(digitalRead(ENCODER_B) == LOW) 
-      EncoderValue++;
+      counter++;
     else 
-      EncoderValue--;
+      counter--;
   }
   
   lastInterruptTime = interruptTime;
+}
+
+int Encoder::getCounts(){
+  return counter;
 }
 
 #endif
