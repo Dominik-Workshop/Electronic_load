efff486dbb969a57e4611043ffd63ef5adee2653
Dominik Pluta <nmarcin87@gmail.com>
2022-08-05 07:08:19 +0200

    input value with decimal point from keypad

--- Electronic_load_code/src/main.cpp
+++ Electronic_load_code/src/main.cpp
@@ -27,6 +27,21 @@
 #include "temperature.hh"
 #include "encoder.hh"
 #include "keypad_config.hh"
+#include "lcd_characters.hh"
+
+void constCurrentMode(LiquidCrystal_I2C& lcd) {
+  lcd.setCursor(0,0);
+  lcd.print("DC LOAD");  
+  lcd.setCursor(0,2);
+  lcd.print("                ");
+  lcd.setCursor(0,2);
+  lcd.print("Set I = ");
+  lcd.setCursor(16,2);
+  lcd.print("    ");
+  lcd.setCursor(14,2);
+  lcd.print("A");
+  lcd.setCursor(0,3);
+}
 
 void setup() {
   CalibrationValues calibrationValues;
@@ -36,12 +51,16 @@ void setup() {
   Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
   Encoder encoder;
   
+  char numbers[10];
+  int index, z;
   char customKey;
-  int value = 0;
+  float value = 0;
   uint32_t time;
 
   lcd.init();
   lcd.backlight();
+  lcd.createChar(degree, degreeSymbol);
+  lcd.createChar(ohm, ohmSymbol);
   adc.begin();
   adc.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.0078125mV
   dac.begin(DAC_ADDRESS);
@@ -52,11 +71,20 @@ void setup() {
   pinInit();
   calibrationValues.readFromEEPROM();
 
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
+  lcd.setCursor(1,0);
+  lcd.print("DC Electronic Load");
+  lcd.setCursor(2,1);
+  lcd.print("Dominik Workshop");
+  lcd.setCursor(8,2);
+  lcd.print("2022");
+  lcd.setCursor(4,3);
+  lcd.print("5A 30V 150W");
+  
+  delay(3000);
 
-  lcd.setCursor(3,2);
-  lcd.print(value);
+  lcd.clear();
+
+  constCurrentMode(lcd);
 
   while(1){
     
@@ -64,20 +92,57 @@ void setup() {
     delay(20);
     digitalWrite(13, LOW);
     delay(20); 
+    lcd.setCursor(16,0);
+    lcd.print(measureTemperature());
+    lcd.write(degree);
+    lcd.print("C");
 
     customKey = keypad.getKey();
     if(customKey == '#'){
       encoder.reset();
       time = millis();
+      index = 0;
+      z = 0;
       while(time + 5000 > millis()){  //exit 5s after last encoder movement
+        customKey = keypad.getKey();
+        if(customKey >= '0' && customKey <= '9'){               //check for keypad number input
+          numbers[index++] = customKey;
+          numbers[index] = '\0';
+          lcd.setCursor(z,3);                              
+          lcd.print(customKey);                              //show number input on LCD
+          z++;
+          time = millis();
+        }
+
+        if(customKey == '*'){                                   //Decimal point
+            numbers[index++] = '.';
+            numbers[index] = '\0';
+            lcd.setCursor(z,3);
+            lcd.print(".");
+            z = z+1;
+        }
+
         if(encoder.rotation()){
           value += encoder.rotation();
-          lcd.setCursor(3,2);
-          lcd.print(value);
-          lcd.print("         ");
+          lcd.setCursor(8,2);
+          lcd.print(value, 3);
+          lcd.print(" ");
           encoder.reset();
           time = millis();
         } 
+        if(customKey == '#') {
+          value = atof(numbers);
+          lcd.setCursor(8,2);
+          lcd.print(value, 3);
+          lcd.print(" ");
+          // if(value < 10) lcd.print(" ");
+          // if(value < 100) lcd.print(" ");
+          // if(value < 1000) lcd.print(" ");
+          lcd.setCursor(0,3);
+          lcd.print("       ");
+          index = 0;
+          z = 0;
+        }
       }
     }
     
