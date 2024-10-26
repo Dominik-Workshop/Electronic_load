e170ef7e909b7b395bf514583ec19fc6afd5afb6
Dominik-Workshop <nmarcin87@gmail.com>
2024-10-26 21:29:04 +0200

    WIP: old changes

--- desktop_app/prj/src/main.cpp
+++ desktop_app/prj/src/main.cpp
@@ -3,9 +3,30 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-#include <qstylefactory.h>
+#include <QStyleFactory>
+#include <QLineEdit>
+#include <QDebug>
 
-int main(int argc, char *argv[]){
+constexpr qreal scaleFactor = 0.6;
+
+void scaleWidget(QWidget *widget, qreal scaleFactor) {
+    // Scale widget size
+    widget->resize(widget->size() * scaleFactor);
+
+    // Scale font size
+    QFont font = widget->font();
+    font.setPointSizeF(font.pointSizeF() * scaleFactor);
+    widget->setFont(font);
+
+    // Recursively scale child widgets
+    foreach (QObject *child, widget->children()) {
+        if (QWidget *childWidget = qobject_cast<QWidget *>(child)) {
+            scaleWidget(childWidget, scaleFactor);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
     QApplication::setStyle(QStyleFactory::create("Fusion"));
     QApplication a(argc, argv);
 
@@ -19,6 +40,30 @@ int main(int argc, char *argv[]){
         }
     }
     Electronic_load_app w;
+    scaleWidget(&w, scaleFactor);
     w.show();
+
+    // Find the central widget first
+    QWidget *centralWidget = w.findChild<QWidget *>("centralwidget");
+    if (centralWidget) {
+        QLineEdit *setCurret = centralWidget->findChild<QLineEdit *>("setCurret", Qt::FindChildrenRecursively);
+        QLineEdit *cutoffVoltage = centralWidget->findChild<QLineEdit *>("cutoffVoltage", Qt::FindChildrenRecursively);
+        QLineEdit *measuredCurrent = centralWidget->findChild<QLineEdit *>("measuredCurrent", Qt::FindChildrenRecursively);
+
+        qDebug() << "setCurret:" << setCurret;
+        qDebug() << "cutoffVoltage:" << cutoffVoltage;
+        qDebug() << "measuredCurrent:" << measuredCurrent;
+
+        if (cutoffVoltage && measuredCurrent) {
+            //setCurret->setFixedWidth(measuredCurrent->width());
+            cutoffVoltage->setFixedWidth(measuredCurrent->width());
+            qDebug() << "scaled";
+        } else {
+            qDebug() << "One or more QLineEdit widgets not found.";
+        }
+    } else {
+        qDebug() << "centralwidget not found.";
+    }
+
     return a.exec();
 }
